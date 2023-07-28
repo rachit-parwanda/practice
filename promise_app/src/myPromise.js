faa88f71d6b93e65222c9904c71a8175e566e266
Rachit Parwanda <rparwanda@Rachits-MacBook-Pro.local>
2023-07-28 19:42:36 +0530

    promise polyfill working, but with only some basic fuctionality

--- promise_app/src/myPromise.js
+++ promise_app/src/myPromise.js
@@ -22,50 +22,62 @@ p
     //
 })
 */
-
 class MyPromise {
-  _resolveFn;
-  _rejectFn;
-  _catchFn;
-
   constructor(executor) {
-    executor(MyPromise.resolve, MyPromise.reject);
+    let _resolveFn;
+    let _rejectFn;
+    let _catchFn;
+
+    // then and catch are class methods, different for each instance
+    this.then = (resolveFn, rejectFn) => {
+      // registering the functions for future use
+      _resolveFn = resolveFn;
+      _rejectFn = rejectFn;
+      return this;
+    };
+
+    this.catch = (catchFn) => {
+      // registering the functions for future use
+      _catchFn = catchFn;
+      return this;
+    };
+
+    // these resolve and reject methods are internal methods,
+    // used to handle resolve / reject callbacks
+    let resolveCallback = (value) => {
+      if (_resolveFn) _resolveFn(value);
+    };
+
+    let rejectCallback = (reason) => {
+      if (_rejectFn) {
+        _rejectFn(reason);
+      } else if (_catchFn) {
+        _catchFn(new Error(reason));
+      }
+    };
+
+    executor(resolveCallback, rejectCallback);
   }
 
-  // then and catch are class methods, different for each instance
-  then(resolveFn, rejectFn) {
-    // registering the functions for future use
-    this._resolveFn = resolveFn;
-    this._rejectFn = rejectFn;
-    return this;
+  // these are static methods for "MyPromise", same for all instances
+  // used to return a resolved / rejected promise
+  static resolve(value) {
+    return new MyPromise((resolve, reject) => {
+      resolve(value);
+    });
   }
-
-  catch(catchFn) {
-    // registering the functions for future use
-    this._catchFn = catchFn;
-    return this;
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => {
+      reject(reason);
+    });
   }
-
-  // resolve and reject are static class methods, same for all instances
-  static resolve = (value) => {
-    console.log("resolve this: ", this);
-    if (MyPromise._resolveFn) this._resolveFn(value);
-  };
-
-  static reject = (reason) => {
-    console.log("reject this: ", this);
-    if (this._rejectFn) {
-      this._rejectFn(reason);
-    } else if (this._catchFn) {
-      this._catchFn(new Error(reason));
-    }
-  };
 }
 
 // initialise
 const p = new MyPromise((resolve, reject) => {
   setTimeout(() => {
     const shouldSucceed = Boolean(Math.floor(Math.random() + 0.5));
+    console.log("should succeed: ", shouldSucceed);
     if (shouldSucceed) {
       resolve("resolved");
     } else {
@@ -77,11 +89,11 @@ const p = new MyPromise((resolve, reject) => {
 // register then and catch handlers
 p.then(
   (value) => {
-    console.log("then value:", value);
+    console.log("then value: ", value);
   },
   (reason) => {
     console.log("then reason:", reason);
   }
 ).catch((error) => {
-  console.log("catch error", error);
+  console.log("catch error: ", error.message);
 });

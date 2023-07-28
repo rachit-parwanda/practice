fc5e92c2e4b93356a31e92e35106e9105bc7d8fc
Rachit Parwanda <rparwanda@Rachits-MacBook-Pro.local>
2023-07-28 23:32:39 +0530

    support for then chaining and corrected the reject case handling

--- promise_app/src/myPromise.js
+++ promise_app/src/myPromise.js
@@ -14,7 +14,7 @@ const p = new Promise((resolve,  reject) => {
 // register the then and catch handlers
 p
 .then((value?) => {
-    // 
+    //
 }, (reason?) => {
     //
 })
@@ -22,38 +22,61 @@ p
     //
 })
 */
+
 class MyPromise {
   constructor(executor) {
-    let _resolveFn;
-    let _rejectFn;
-    let _catchFn;
+    let _callbacks = [];
+    let _catchFns = [];
 
     // then and catch are class methods, different for each instance
     this.then = (resolveFn, rejectFn) => {
       // registering the functions for future use
-      _resolveFn = resolveFn;
-      _rejectFn = rejectFn;
+      _callbacks.push({ type: "resolve", fn: resolveFn });
+      if (rejectFn) _callbacks.push({ type: "reject", fn: rejectFn });
+
       return this;
     };
 
     this.catch = (catchFn) => {
       // registering the functions for future use
-      _catchFn = catchFn;
+      _catchFns.push(catchFn);
       return this;
     };
 
     // these resolve and reject methods are internal methods,
     // used to handle resolve / reject callbacks
     let resolveCallback = (value) => {
-      if (_resolveFn) _resolveFn(value);
+      _callbacks.reduce((prevReturn, currCallback) => {
+        if (currCallback.type === "resolve") {
+          return currCallback.fn(prevReturn);
+        }
+        return prevReturn;
+      }, value);
     };
 
     let rejectCallback = (reason) => {
-      if (_rejectFn) {
-        _rejectFn(reason);
-      } else if (_catchFn) {
-        _catchFn(new Error(reason));
+      let rejectIdx = _callbacks.findIndex(
+        (callback) => callback.type === "reject"
+      );
+
+      if (rejectIdx === -1) {
+        _catchFns[0](new Error(reason));
+        return;
       }
+
+      let foundReject = false;
+      _callbacks.reduce((prevReturn, currCallback, idx) => {
+        if (!foundReject && currCallback.type === "reject") {
+          foundReject = true;
+          return currCallback.fn(prevReturn);
+        }
+
+        if (foundReject && currCallback.type === "resolve") {
+          return currCallback.fn(prevReturn);
+        }
+
+        return prevReturn;
+      }, reason);
     };
 
     executor(resolveCallback, rejectCallback);
@@ -83,17 +106,67 @@ const p = new MyPromise((resolve, reject) => {
     } else {
       reject("rejected");
     }
-  }, 1500);
+  }, 500);
 });
 
 // register then and catch handlers
-p.then(
-  (value) => {
-    console.log("then value: ", value);
-  },
-  (reason) => {
-    console.log("then reason:", reason);
-  }
-).catch((error) => {
-  console.log("catch error: ", error.message);
-});
+p.then((value) => {
+  console.log("then value: ", value);
+})
+  .then(
+    (value) => {
+      console.log("then2 value: ", value);
+      return "resolved2";
+    },
+    (reason) => {
+      console.log("then2 reason:", reason);
+      return "testing2";
+    }
+  )
+  .then((value) => {
+    console.log("then3 value: ", value);
+  })
+  .catch((error) => {
+    console.log("catch error: ", error.message);
+  });
+
+//
+//
+//
+//
+
+// const np = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const shouldSucceed = Boolean(Math.floor(Math.random() + 0.5));
+//     console.log("should succeed: ", shouldSucceed);
+//     if (shouldSucceed) {
+//       resolve("resolved");
+//     } else {
+//       reject("rejected");
+//     }
+//   }, 500);
+// });
+
+// np.then((value) => {
+//   console.log("then value: ", value);
+//   return "resolved1";
+// })
+//   .then(
+//     (value) => {
+//       console.log("then2 value: ", value);
+//       return "resolved2";
+//     },
+//     (reason) => {
+//       console.log("then2 reason:", reason);
+//       return "rejected1";
+//     }
+//   )
+//   .then((value) => {
+//     console.log("then3 value: ", value);
+//   })
+//   .catch((error) => {
+//     console.log("catch error: ", error);
+//   })
+//   .catch((error) => {
+//     console.log("catch2 error: ", error);
+//   });

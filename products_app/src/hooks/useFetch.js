d92670a8511144b1da9b12ebc1a745f023dea39e
Rachit Parwanda <rparwanda@Rachits-MacBook-Pro.local>
2023-08-04 00:23:09 +0530

    integrated axios

--- products_app/src/hooks/useFetch.js
+++ products_app/src/hooks/useFetch.js
@@ -1,30 +1,42 @@
 import { useCallback, useState } from "react";
+import axios from "axios";
 
 export const useFetch = () => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMsg, setErrorMsg] = useState(null);
 
-  const fetchData = useCallback((url, options) => {
+  const fetchData = useCallback(async (url, options) => {
     setIsLoading(true);
     setErrorMsg(null);
 
-    fetch(url, options)
-      .then((res) => {
-        if (!res.ok)
-          throw new Error(`unable to fetch data.\nStatus ${res.status}`);
-        else return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsLoading(false);
-        setErrorMsg(null);
-      })
-      .catch((err) => {
-        setData(null);
-        setIsLoading(false);
-        setErrorMsg(err.message);
-      });
+    try {
+      const res = await axios.get(url, options);
+      setData(res.data);
+      setIsLoading(false);
+      setErrorMsg(null);
+    } catch (error) {
+      setData(null);
+      setIsLoading(false);
+      setErrorMsg(error.message);
+    }
+
+    // fetch(url, options)
+    //   .then((res) => {
+    //     if (!res.ok)
+    //       throw new Error(`unable to fetch data.\nStatus ${res.status}`);
+    //     else return res.json();
+    //   })
+    //   .then((data) => {
+    //     setData(data);
+    //     setIsLoading(false);
+    //     setErrorMsg(null);
+    //   })
+    //   .catch((err) => {
+    //     setData(null);
+    //     setIsLoading(false);
+    //     setErrorMsg(err.message);
+    //   });
   }, []);
 
   return {
